1ec8739732822f6e4ca83d3b6a70024b05214003
Afnitha <afnithaaa@gmail.com>
2026-08-08 02:47:44 +0400

    Add Terraform infrastructure as code

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "flask_sg" {
+  name        = "docker-flask-production-sg"
+  description = "Security group for Docker Flask application"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Flask application"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "docker-flask-production-sg"
+  }
+}
+resource "aws_instance" "flask_server" {
+  instance_type = "t3.micro"
+  ami           = "ami-096f5760b00bcd95c"
+
+  subnet_id = "subnet-035fbcc2eda343d18"
+
+  key_name = "Oregonkey"
+
+  tags = {
+    Name = "docker-flask-production-server"
+  }
+}
