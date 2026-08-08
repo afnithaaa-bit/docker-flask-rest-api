74f1da908b77911cb76abb8d96cf434031bbc363
Afnitha <afnithaaa@gmail.com>
2026-08-08 05:51:01 +0400

    Secure environment variables and update Terraform instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,14 +53,14 @@ resource "aws_security_group" "flask_sg" {
   }
 }
 resource "aws_instance" "flask_server" {
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   ami           = "ami-096f5760b00bcd95c"
 
   subnet_id = "subnet-035fbcc2eda343d18"
 
   key_name = "Oregonkey"
 
-  tags = {
+   tags = {
     Name = "docker-flask-production-server"
   }
 }
